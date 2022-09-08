b120aa91b4713fba995273ac497d6c3a51477671
Konrad Kazusek <64148572+kkvzx@users.noreply.github.com>
2022-09-08 19:22:44 +0200

    added comments

--- src/index.js
+++ src/index.js
@@ -18,12 +18,13 @@ const path = require("path");
     )
   );
 
-  // console.log(new Date().toISOString());
+  console.log(new Date().toISOString());
   console.log(count(users, mobileDevices, iotDevices));
-  // console.log(new Date().toISOString());
+  console.log(new Date().toISOString());
 })();
 
 function count(users, mobileDevices, iotDevices) {
+  // First Function (description below)
   // checks how many IOT DEVICES is connected to every device and saves this data in mobile_devices object[]
   const howManyIotIsConnectedToSingleDevice = () => {
     return mobileDevices.map((singleMobileDevice) => {
@@ -34,6 +35,7 @@ function count(users, mobileDevices, iotDevices) {
     });
   };
 
+  // Second Function (description below)
   // Checks how many mobile devices is connected to Single User and thanks to data from howManyIotIsConnectedToSingleDevice()
   // it calculates the sum of iot devices in every mobile devices used by a user
   const howManyMobileDevicesIsConnectedToSingleUser = (
@@ -53,8 +55,7 @@ function count(users, mobileDevices, iotDevices) {
     });
   };
 
-  // Takes the data from howManyMobileDevicesIsConnectedToSingleUser and creacts new object[] with name and Total devices only
-  // How many iot devices have been ever owned by users with the same name
+  // Third Function (description below)
   const numberOfIotDevicesUsersWithTheSameName = (usersWithTotalIot) => {
     let namesAndIotDevices = {};
 
@@ -71,7 +72,7 @@ function count(users, mobileDevices, iotDevices) {
         (namesAndIotDevices[user.name] || 0) +
         user.iotDevicesConnectedToSingleUser;
     });
-    // saves the result
+    // saves the result as object
     let result = Object.keys(namesAndIotDevices).map((k) => {
       return {
         name: k,
@@ -82,12 +83,46 @@ function count(users, mobileDevices, iotDevices) {
     return result;
   };
 
+  // Fourth function (description below)
+  const convertObjectToString = (namesWithIotDevices) => {
+    const result = namesWithIotDevices.map(
+      ({ name, iotDevicesConnectedToUserName }) => {
+        const text = `${name} => ${iotDevicesConnectedToUserName}`;
+        return text;
+      }
+    );
+    return result;
+  };
+
   const mobileDevicesWithIot = howManyIotIsConnectedToSingleDevice();
   const usersWithTotalIot =
     howManyMobileDevicesIsConnectedToSingleUser(mobileDevicesWithIot);
-
   const namesWithIotDevices =
     numberOfIotDevicesUsersWithTheSameName(usersWithTotalIot);
+  const textResult = convertObjectToString(namesWithIotDevices);
 
-  return namesWithIotDevices;
+  return textResult;
 }
+
+// Function counts contain four functions:
+//
+//-->howManyIotIsConnectedToSingleDevice()
+//  This function determines how many Iot Devices is connected to a single mobile device.
+// it maps over all of mobile devices and iot devices looking for matching mobile id. Later on it counts a number of iot devices for every mobile device.
+// Function add up this data to object ( how many iot devices was connected ) and returns it.
+//
+//-->howManyMobileDevicesIsConnectedToSingleUser(mobileDevicesWithIot)
+//  Function accepts modified mobileDevices object. Function follow the same pattern as howManyIotIsConnectedToSingleDevice().
+// Function checks how many mobile devices is connected to Single User
+// it calculates the sum of iot devices in every mobile devices used by a user
+//
+//-->numberOfIotDevicesUsersWithTheSameName(usersWithTotalIot)
+//  Takes the data from howManyMobileDevicesIsConnectedToSingleUser and creacts new object[] with name and Total devices only
+// How many iot devices have been ever owned by users with the same name
+// it merges objects with repeating names and calculating sums of iot devices for everyone.
+//
+// -->convertObjectToString(namesWithIotDevices)
+//  Function simply convert object into array of strings to match the example.
+
+// NOTE:
+//  I completed this task in four function to maximize readability.
